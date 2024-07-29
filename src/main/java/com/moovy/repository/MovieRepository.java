5760893d91dd97fdda18742de907c047d25e18dc
shashanksp <Shashankprince523@gmail.com>
2024-07-29 20:01:30 +0530

    removed @Query Annotation

--- src/main/java/com/moovy/repository/MovieRepository.java
+++ src/main/java/com/moovy/repository/MovieRepository.java
@@ -3,12 +3,10 @@ package com.moovy.repository;
 
 import com.moovy.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
-    @Query("SELECT m FROM Movie m WHERE m.title = ?1")
     List<Movie> getMovieByTitle(String title);
 }
